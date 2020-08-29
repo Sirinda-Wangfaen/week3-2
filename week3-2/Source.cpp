9b2d0928ed13baedc144f8d46c8cf15bf3e4f2ea
Sirinda Wangfan <63010979@kmitl.ac.th>
2020-08-29 21:56:33 +0700

    -

--- week3-2/Source.cpp
+++ week3-2/Source.cpp
@@ -5,19 +5,24 @@ int main()
     int row, c = 1, n, i, j;
     printf("Input number of rows: ");
     scanf_s("%d", &row);
-    for (i = 0; i < row; i++)
-    {
-        for (n = 1; n <= row - i; n++)
-            printf("  ");
-        for (j = 0; j <= i; j++)
+    if (row > 0) {
+        for (i = 0; i < row; i++)
         {
-            if (j == 0 || i == 0)
-                c = 1;
-            else
-                c = c * (i - j + 1) / j;
-            printf("% 4d", c);
+            for (n = 1; n <= row - i; n++)
+                printf("  ");
+            for (j = 0; j <= i; j++)
+            {
+                if (j == 0 || i == 0)
+                    c = 1;
+                else
+                    c = c * (i - j + 1) / j;
+                printf("% 4d", c);
+            }
+            printf("\n");
         }
-        printf("\n");
+    }
+    else {
+        printf("Error");
     }
     return 0;
 }
